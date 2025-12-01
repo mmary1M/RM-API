ad51a6b4893154765bfd1828f70fe76b23d70838
mmary1M <mmmaria.morais1@gmail.com>
2025-12-01 10:25:53 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import { useEffect, useState } from 'react'
 import s from './App.module.css'
 import {api} from './api/api'
-import { card } from './components/card'
-const [searchName, setSearchname] = useState("")
-const [searchPage, setsearchPage] = useStste("")
-
+import { Card } from './components/card'
 
 function App() {
   const [data, setData] = useState([])
-  const [searchName, setSearchname] = useState("")
+  const [searchName, setSearchName] = useState("")
   const [searchPage, setSearchPage] = useState("")
 
 
@@ -24,12 +21,15 @@ function App() {
     <>
       <h1 className={s.title}>Rick and Morty Characters</h1>
       <main>
+        <div style={{display: "flex", flexWrap:"flexWrap", gap:"20px", alignItrms:"center", justifyConten:"center"}}> 
         <input type="text" value={searchPage} onChange={(e) => setSearchPage(e.target.value)} placeholder="1/42"/>
-        <input type="text" value={searchName} onChange={(e) => setSearchname(e.target.value)} placeholder="Procure um personagem"/>
+        <input type="text" value={searchName} onChange={(e) => setSearchName(e.target.value)} placeholder="Procure um personagem"/>
+        </div>
+        
       <div className={s.wrapCards}>
-        {data.map((item) =>{
+        {data.map((item, index) =>{
           return(
-            <div key={index}>
+            <div key={index} >
               <Card image={item.image} name={item.name} species={item.species}/>
 
             </div>
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -11,14 +11,12 @@ import s from './card.module.css'
 //  )
 // }
 //pode usar esse doois tipos mais esse com image, name, species é mais usado em empresas
-export const card = (image, name, species) => {
+export const Card = ({image, name, species}) => {
     return(
-        <>
-            <img src={image} alt={name} />
-              <h2>{name}</h2>
-              <p>{species}</p>
-        
-        </>
+        <div className={s.cardBackground}>
+        <img className={s.cardImage} src={image} alt={name} />
+        <h2 className={s.cardTitle}>{name}</h2>
+         <p className={s.cardText}>{species}</p>
+        </div>
     )
-   
 }
